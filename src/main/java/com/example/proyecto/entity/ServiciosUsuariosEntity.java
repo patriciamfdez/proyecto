ddd18966e2b67b76e5cd82def008f5b042596914
patriciamfdez <patriciamaldonado410@gmail.com>
2022-06-15 13:29:35 +0200

    cambios

--- src/main/java/com/example/proyecto/entity/ServiciosUsuariosEntity.java
+++ src/main/java/com/example/proyecto/entity/ServiciosUsuariosEntity.java
@@ -1,4 +1,13 @@
 package com.example.proyecto.entity;
 
+import java.util.Date;
+
 public class ServiciosUsuariosEntity {
+    private Integer id;
+    private String nombre_usuario;
+    private String estado_reserva;
+    private Date fecha_creacion;
+    private String nombre_servicio;
+    private Date fecha_inicio_reserva;
+    private Date fecha_final_reserva;
 }
