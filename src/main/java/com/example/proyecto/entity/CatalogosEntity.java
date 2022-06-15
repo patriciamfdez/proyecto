eb706368f42197e9d8dc85d140fc5e051012ab3b
celiamorenovillamayor <celia_174@hotmail.com>
2022-06-15 13:24:48 +0200

    Constructores, getter y setter

--- src/main/java/com/example/proyecto/entity/CatalogosEntity.java
+++ src/main/java/com/example/proyecto/entity/CatalogosEntity.java
@@ -1,4 +1,20 @@
 package com.example.proyecto.entity;
 
 public class CatalogosEntity {
+
+    private String tipoServicio;
+
+    //Constructores
+    public CatalogosEntity(String tipoServicio) {
+        this.tipoServicio = tipoServicio;
+    }
+    //GetterSetter
+
+    public String getTipoServicio() {
+        return tipoServicio;
+    }
+
+    public void setTipoServicio(String tipoServicio) {
+        this.tipoServicio = tipoServicio;
+    }
 }
--- src/main/java/com/example/proyecto/entity/FacturasEntity.java
+++ src/main/java/com/example/proyecto/entity/FacturasEntity.java
@@ -1,4 +1,44 @@
 package com.example.proyecto.entity;
+import java.util.Date;
 
 public class FacturasEntity {
+
+    private Date fechaCreacion;
+    private float totalPago;
+    private String estadoPago;
+
+    //Constructor
+
+    public FacturasEntity(Date fechaCreacion, float totalPago, String estadoPago) {
+        this.fechaCreacion = fechaCreacion;
+        this.totalPago = totalPago;
+        this.estadoPago = estadoPago;
+    }
+
+    //GetterSetter
+
+
+    public Date getFechaCreacion() {
+        return fechaCreacion;
+    }
+
+    public void setFechaCreacion(Date fechaCreacion) {
+        this.fechaCreacion = fechaCreacion;
+    }
+
+    public float getTotalPago() {
+        return totalPago;
+    }
+
+    public void setTotalPago(float totalPago) {
+        this.totalPago = totalPago;
+    }
+
+    public String getEstadoPago() {
+        return estadoPago;
+    }
+
+    public void setEstadoPago(String estadoPago) {
+        this.estadoPago = estadoPago;
+    }
 }
