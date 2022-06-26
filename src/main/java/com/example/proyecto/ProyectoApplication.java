9e001d470b4a693384fde38781140677abaa78a9
patriciamfdez <patriciamaldonado410@gmail.com>
2022-06-26 12:19:43 +0200

    cambios

--- src/main/java/com/example/proyecto/ProyectoApplication.java
+++ src/main/java/com/example/proyecto/ProyectoApplication.java
@@ -1,13 +1,42 @@
 package com.example.proyecto;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @SpringBootApplication
-public class ProyectoApplication {
+public class ProyectoApplication extends WebSecurityConfigurerAdapter {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProyectoApplication.class, args);
 	}
 
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+
+		http.authorizeRequests()
+				.antMatchers("/login")
+				.permitAll()
+				.antMatchers("/**")
+				.hasAnyRole("BASICO")
+				.and()
+				.formLogin()
+				.loginPage("/login")
+				.defaultSuccessUrl("/hola")
+				.failureUrl("/login?error=true")
+				.permitAll()
+				.and()
+				.logout()
+				.logoutSuccessUrl("/login?logout=true")
+				.invalidateHttpSession(true)
+				.permitAll();
+	}
+
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication().withUser("root").password("1234").roles("BASICO");
+	}
 }
