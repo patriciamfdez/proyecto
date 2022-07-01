e1ac02126f8da3808866c2806ba6175e90de8597
franc <noble028@gmail.com>
2022-07-01 12:01:52 +0200

    Prueba

---
+++ src/main/java/com/example/proyecto/data/entity/DogsEntity.java
@@ -0,0 +1,122 @@
+package com.example.proyecto.data.entity;
+
+public class DogsEntity {
+    private Integer id;
+    private String name;
+    private float weightKg;
+    private String age;
+    private String gender;
+    private String race;
+    private boolean microchip;
+    private boolean sterilized;
+    private String infAdditional;
+    private boolean compatible; // Este atributo identifica si el perro puede pasar con otros perros (true) o si tiene que pasear solo (false)
+    private String allergies;
+
+    // Constructor
+
+    public DogsEntity(Integer id, String name, float weightKg, String age, String gender, String race, boolean microchip, boolean sterilized, String inf_adiocional, boolean compatible, String allergies) {
+        this.id = id;
+        this.name = name;
+        this.weightKg = weightKg;
+        this.age = age;
+        this.gender = gender;
+        this.race = race;
+        this.microchip = microchip;
+        this.sterilized = sterilized;
+        this.infAdditional = infAdditional;
+        this.compatible = compatible;
+        this.allergies = allergies;
+    }
+
+
+    //Getters y Setters
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setNombre(String nombre) {
+        this.name = name;
+    }
+
+    public float getWeightKg() {
+        return weightKg;
+    }
+
+    public void setWeightKg(float weightKg) {
+        this.weightKg = weightKg;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getRace() {
+        return race;
+    }
+
+    public void setRace(String race) {
+        this.race = race;
+    }
+
+    public boolean isMicrochip() {
+        return microchip;
+    }
+
+    public void setMicrochip(boolean microchip) {
+        this.microchip = microchip;
+    }
+
+    public boolean isSterilized() {
+        return sterilized;
+    }
+
+    public void setSterilized(boolean esterilizado) {
+        this.sterilized = sterilized;
+    }
+
+    public String getInfAdditional() {
+        return infAdditional;
+    }
+
+    public void setInfAdditional(String infAdditional) {
+        this.infAdditional = infAdditional;
+    }
+
+    public boolean isCompatible() {
+        return compatible;
+    }
+
+    public void setCompatible(boolean compatible) {
+        this.compatible = compatible;
+    }
+
+    public String getAllergies() {
+        return allergies;
+    }
+
+    public void setAllergies(String allergies) {
+        this.allergies = allergies;
+    }
+}
---
+++ src/main/java/com/example/proyecto/data/entity/ServicesUsersEntity.java
@@ -0,0 +1,85 @@
+package com.example.proyecto.data.entity;
+
+import java.util.Date;
+
+public class ServicesUsersEntity {
+    private Integer id;
+    private String username;
+    private String BookingStatus;
+    private Date CreationDate;
+    private String ServiceName;
+    private Date BookingStartDate;
+    private Date BookingLastDate;
+
+    // Constructores
+
+    public ServicesUsersEntity(Integer id, String username, String BookingStatus, Date CreationDate, String ServiceName, Date BookingStartDate, Date BookingLastDate) {
+        this.id = id;
+        this.username = username;
+        this.BookingStatus = BookingStatus;
+        this.CreationDate = CreationDate;
+        this.ServiceName = ServiceName;
+        this.BookingStartDate = BookingStartDate;
+        this.BookingLastDate = BookingLastDate;
+    }
+
+
+    // Getter and Setters
+
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getBookingStatus() {
+        return BookingStatus;
+    }
+
+    public void setBookingStatus(String bookingStatus) {
+        this.BookingStatus = bookingStatus;
+    }
+
+    public Date getCreationDate() {
+        return CreationDate;
+    }
+
+    public void setCreationDate(Date creationDate) {
+        this.CreationDate = creationDate;
+    }
+
+    public String getServiceName() {
+        return ServiceName;
+    }
+
+    public void setServiceName(String serviceName) {
+        this.ServiceName = serviceName;
+    }
+
+    public Date getBookingStartDate() {
+        return BookingStartDate;
+    }
+
+    public void setBookingStartDate(Date bookingStartDate) {
+        this.BookingStartDate = bookingStartDate;
+    }
+
+    public Date getBookingLastDate() {
+        return BookingLastDate;
+    }
+
+    public void setBookingLastDate(Date bookingLastDate) {
+        this.BookingLastDate = bookingLastDate;
+    }
+}
