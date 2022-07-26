d3d7bdc5e2fc4bf1b19d6eeff6f305675e905f5b
patriciamfdez <patriciamaldonado410@gmail.com>
2022-07-26 13:36:07 +0200

    cambios

--- src/main/java/com/example/proyecto/service/DaycareService.java
+++ src/main/java/com/example/proyecto/service/DaycareService.java
@@ -9,6 +9,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+
 @Service
 public class DaycareService extends AbstractBusinessService<Daycare, Integer, DaycareDTO, DaycareRepository, DaycareMapper> {
 
